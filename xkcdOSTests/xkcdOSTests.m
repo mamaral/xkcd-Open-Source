216f4987b1c0a7fdc5ad135bb888885d57b8cd6f
Mike Amaral <mike.amaral@ping4.com>
2015-05-20 20:38:41 -0400

    Added initial test and new test target.

---
+++ xkcdOSTests/xkcdOSTests.m
@@ -0,0 +1,30 @@
+//
+//  xkcdOSTests.m
+//  xkcdOSTests
+//
+//  Created by Mike on 5/20/15.
+//  Copyright (c) 2015 Mike Amaral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface xkcdOSTests : XCTestCase
+
+@end
+
+@implementation xkcdOSTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testTest {
+    XCTAssert(YES);
+}
+
+@end
