1bd0600c5b418b147a430f2caf7af5e15927b00a
Mike Amaral <mike.amaral@ping4.com>
2015-05-22 19:22:17 -0400

    Initial data manager tests.

---
+++ xkcdOSTests/DataManagerTests.m
@@ -0,0 +1,26 @@
+//
+//  DataManagerTests.m
+//  xkcd Open Source
+//
+//  Created by Mike on 5/22/15.
+//  Copyright (c) 2015 Mike Amaral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface DataManagerTests : XCTestCase
+
+@end
+
+@implementation DataManagerTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+@end
