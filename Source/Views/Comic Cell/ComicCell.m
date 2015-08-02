9a2e15f96e501688772925cc4dd9e3b9f93466ab
Mike Amaral <mike.amaral@ping4.com>
2015-08-02 18:28:52 -0400

    Updated padding on fav icon on cells.

--- Source/Views/Comic Cell/ComicCell.m
+++ Source/Views/Comic Cell/ComicCell.m
@@ -74,7 +74,7 @@ static CGFloat const kFavoriteIconSize = 55.0;
     [self.maskView fillSuperview];
     [self.numberLabel anchorBottomRightWithRightPadding:4 bottomPadding:4 width:kComicCellNumberLabelWidth height:kComicCellNumberLabelWidth];
     [self.highlightedMask fillSuperview];
-    [self.favoritedIcon anchorTopLeftWithLeftPadding:2 topPadding:2 width:kFavoriteIconSize height:kFavoriteIconSize];
+    [self.favoritedIcon anchorTopLeftWithLeftPadding:0 topPadding:0 width:kFavoriteIconSize height:kFavoriteIconSize];
 }
 
 - (void)setComic:(Comic *)comic {
